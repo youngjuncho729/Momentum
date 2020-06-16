1606ccdd0d3ef95d5fc298bbeb4025c6a831bccb
Youngjun Cho <youngjun.cho@mail.utoronto.ca>
2020-06-17 01:04:08 +0900

    able to save and remove TODO list

--- todo.js
+++ todo.js
@@ -4,15 +4,41 @@ const todoList = document.querySelector('.js-todoList');
 
 const TODOS_LS = 'toDos';
 
+let todos = [];
+
+function saveTodos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(todos));
+}
+
+function deleteTodo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    todoList.removeChild(li);
+    const cleanTodos = todos.filter(todo => {
+        return todo.id !== parseInt(li.id);
+    });
+    todos = cleanTodos;
+    saveTodos();
+}
+
 function paintTodos(text){
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
-    delBtn.innerText = "❌";
     const span = document.createElement('span');
+    const newId = todos.length + 1;
+    delBtn.innerText = "❌";
+    delBtn.addEventListener('click', deleteTodo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     todoList.appendChild(li);
+    const todoObj = {
+        text: text,
+        id: newId
+    }
+    todos.push(todoObj);
+    saveTodos();
 }
 
 function handleSubmit(event){
@@ -25,7 +51,10 @@ function handleSubmit(event){
 function loadTodos(){
     const todos = localStorage.getItem(TODOS_LS);
     if (todos !== null){
-
+        const parsedTodos = JSON.parse(todos);
+        parsedTodos.forEach(todo => {
+            paintTodos(todo.text);
+        });
     }
 }
 
