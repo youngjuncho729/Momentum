02d87096f64bf51e4900a1d629a9f43b2d0f5b7b
Youngjun Cho <youngjun.cho@mail.utoronto.ca>
2020-06-18 01:21:00 +0900

    change background images

---
+++ bg.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body');
+
+const IMG_NUM = 5;
+
+function paintImg(imgNum){
+    const image = new Image();
+    image.src = `images/${imgNum + 1}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image)
+}
+
+function genRandom(){
+    const number = Math.floor(Math.random() * IMG_NUM);
+    return number;
+}
+
+function init(){
+    const randomNum = genRandom();
+    console.log(randomNum);
+    paintImg(randomNum);
+}
+
+init();
